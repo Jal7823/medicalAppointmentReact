0678447a0226f99a015750df2b80b6754bdd7d85
jal7823 <Jal7823@gmail.com>
2023-11-01 10:04:40 -0300

    feat: :sparkles: Control of conections
    Control of conections in Hospitals

--- src/pages/Specialty.jsx
+++ src/pages/Specialty.jsx
@@ -3,36 +3,52 @@ import { getAllData } from "../utils/crud";
 
 function Specialty() {
   const [Specialty, setSpecialty] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await getAllData("users/specialty");
-      setSpecialty(res.data);
+      try {
+        const res = await getAllData("users/specialty");
+        setSpecialty(res.data);
+        setLoading(false);
+      } catch (e) {
+        setError(error.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
 
   return (
-    <div className="container flex justify-center ">
-      <table className="min-w-34">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Especialidad</th>
-            <th className="px-4 py-2">Icon</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Specialty.map((el) => (
-            <tr key={el.id}>
-              <td className="border px-4 py-2">{el.name}</td>
-              <td className="border px-4 py-2 text-center text-2xl">
-                <i className={`${el.icon} text-center text-red-700`}></i>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      {loading ? (
+        <div></div>
+      ) : error ? (
+        <div></div>
+      ) : (
+        <div className="container flex justify-center ">
+          <table className="min-w-34">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Especialidad</th>
+                <th className="px-4 py-2">Icon</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Specialty.map((el) => (
+                <tr key={el.id}>
+                  <td className="border px-4 py-2">{el.name}</td>
+                  <td className="border px-4 py-2 text-center text-2xl">
+                    <i className={`${el.icon} text-center text-red-700`}></i>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
   );
 }
 
